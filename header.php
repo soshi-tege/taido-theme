7b36ef5ec5c43401250c49532172561d3e1975e4
soshi-tege <sh.cho.zhi038099@gmail.com>
2026-08-17 20:50:15 +0900

    Fix: Undo the recent push (it did not resolve the issue and added more spacing)

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
     </script>
     <?php endif; ?>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php ?>
     <?php wp_head(); ?>
 </head>
