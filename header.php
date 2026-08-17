4f09f96e8bd9bd9c1db002dd1b835b112f914c71
soshi-tege <sh.cho.zhi038099@gmail.com>
2026-08-17 20:38:17 +0900

    Fix: Prevent soem browsers from displaying empty spaces (notches) by view-portfit=cover and safe-area-inset paddings

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
     </script>
     <?php endif; ?>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <?php ?>
     <?php wp_head(); ?>
 </head>
